53c56d5728409d08e0e83ea06eb50f4ae0c7a458
BoNGoNAWT <58984645+BoNGoNAWT@users.noreply.github.com>
2020-07-12 13:58:18 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -30,7 +30,18 @@ async def on_ready():
 async def on_command_error(ctx, error):
     pass
 
-
+@Bot.event
+async def on_voice_state_update(member,before,after):
+    if after.channel.id == 728592023829086278:
+        for guild in Bot.guilds:
+            maincategory = discord.utils.get(guild.categories, id=584629580057542656)
+            channel2 = await guild.create_voice_channel(name=f"Канал {member.display_name}",category = maincategory)
+            await channel2.set_permissions(member,connect=True,mute_members=True,move_members=True,manage_channels=True)
+            await member.move_to(channel2)
+            def check(x,y,z):
+                return len(channel2.members) == 0
+            await Bot.wait_for('voice_state_update',check=check)
+            await channel2.delete()
 
 @Bot.event
 async def on_member_join(member):
@@ -181,18 +192,6 @@ async def send(ctx, member: discord.Member, *, msg):
     await member.send('{}'.format(msg))
     await ctx.send('Сообщение отправлено')
     
-@Bot.event
-async def on_voice_state_update(member,before,after):
-    if after.channel.id == 728592023829086278:
-        for guild in Bot.guilds:
-            maincategory = discord.utils.get(guild.categories, id=584629580057542656)
-            channel2 = await guild.create_voice_channel(name=f"Канал {member.display_name}",category = maincategory)
-            await channel2.set_permissions(member,connect=True,mute_members=True,move_members=True,manage_channels=True)
-            await member.move_to(channel2)
-            def check(x,y,z):
-                return len(channel2.members) == 0
-            await Bot.wait_for('voice_state_update',check=check)
-            await channel2.delete()
 
 @Bot.command()
 async def ping(ctx):
